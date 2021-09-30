86650fb62a06f2e18260be259e69af08719f3d3f
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2021-09-30 16:22:20 +0600

    process structure done

--- Assignment/processStruct.c
+++ Assignment/processStruct.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<sys/wait.h>
 #include<errno.h>
+#include<signal.h>
 
 int main(){
     pid_t child1,child2,childnofc2;
@@ -16,26 +17,48 @@ int main(){
     else if(child1 == 0){
         //pipe reader
         printf("child1 %u, parent : %u\n",getpid(),getppid());
+        for(int i = 0;i<20;i--){
+            printf("Listening...\n");
+            
+        }
         return 0;
     }
 
+
+    //create second child process
     child2 = fork();
     if(child2 == -1){
         printf("Can not create Reader Process\n");
         return 1;
     }
     else if(child2 == 0){
-        //pipe reader
-        printf("child2 %u, parent : %u\n",getpid(),getppid());
-        for(int i = 0;i<1;i--){
-            printf("child2 %u, parent : %u\n",getpid(),getppid());
-        }
+        
+        int n;
+
+        for(int i = 0;i<10;i++){
+            childnofc2 = fork();
+            if(childnofc2 == -1){
+                printf("Can not create child process: %u \t iteration: %d \n",childnofc2,i+1);
+            }
+            else if(childnofc2 == 0){
+                
+                printf("Child PID: %u \t parent PID: %u \t Child iteration: %d \n",getpid(),getppid(),i+1);
+                // sleep(1);
+                exit(0);
+            }
+
+            wait(NULL);
+        
+        } 
+
+        kill(child1,SIGTERM);
 
         return 0;
     }
 
     wait(NULL);
     wait(NULL);
+
 
     return 0;
 }
